381fd63fb17333fa6ecca348afba4c6962e76407
sabarinathan <sabarinathan.p2021@vitstudent.ac.in>
2024-11-10 23:14:15 +0530

    D2: Implemented ConsoleWindow class methods

--- ConsoleWindow.java
+++ ConsoleWindow.java
@@ -3,21 +3,21 @@ package Adda.Boys.PaintSoftware;
 public class ConsoleWindow extends Window {
     @Override
     public void open() {
-        System.out.println("Opening the Console Window");
+        System.out.println("Opening Console Window");
     }
 
     @Override
     public void close() {
-        System.out.println("Closing the Console Window");
+        System.out.println("Closing Console Window");
     }
 
     @Override
     public void move() {
-        System.out.println("Moving the Console Window");
+        System.out.println("Moving Console Window");
     }
 
     @Override
     public void display() {
-        System.out.println("Displaying the Console Window");
+        System.out.println("Displaying  Console Window");
     }
 }
