8a9e2ac567d9e3604d9fefd538aea5aab45b8fb3
sabarinathan <sabarinathan.p2021@vitstudent.ac.in>
2024-11-10 23:16:14 +0530

    D3: Implemented DrawingContext class methods

--- DrawingContext.java
+++ DrawingContext.java
@@ -2,20 +2,20 @@ package Adda.Boys.PaintSoftware;
 
 public class DrawingContext {
     public void setPoint() {
-        System.out.println("Setting point in DrawingContext");
+        System.out.println("Setting the point in DrawingContext");
     }
 
     public void clearScreen() {
-        System.out.println("Clearing screen in DrawingContext");
+        System.out.println("Clearing the screen in DrawingContext");
     }
 
     public int getVerticalSize() {
-        System.out.println("Getting vertical size in DrawingContext");
+        System.out.println("Getting the vertical size in DrawingContext");
         return 0; // example return value
     }
 
     public int getHorizontalSize() {
-        System.out.println("Getting horizontal size in DrawingContext");
+        System.out.println("Getting the horizontal size in DrawingContext");
         return 0; // example return value
     }
 }
